69ee40f549cd2094628b8b2c53c23b5dfcdc6dee
taeheon95 <dalo952003@gmail.com>
2021-06-27 15:01:44 +0900

    리덕스 thunk로 비동기 처리 성공

---
+++ src/modules/workspaces.js
@@ -0,0 +1,40 @@
+import AppInitData from '../DummyData/AppInitData';
+
+const ADD_WORKSPACE = 'workspace/ADD_WORKSPACE';
+const DELETE_WORKSPACE = 'workspace/DELETE_WORKSPACE';
+// const CHANGE_WORKSPACE_NAME = 'workspace/CHANGE_WORKSPACE_NAME';
+const CHANGE_WORKSPACE_FAV = 'workspace/CHANGE_WORKSPACE_FAV';
+
+export const addWorkspace = workspace => ({ type: ADD_WORKSPACE, workspace });
+export const deleteWorkspace = ws_id => ({ type: DELETE_WORKSPACE, ws_id });
+// export const changeWorkspaceName = () => ({type:CHANGE_WORKSPACE_NAME, ws_id});
+export const changeWorkspaceFav = ws_id => ({ type: CHANGE_WORKSPACE_FAV, ws_id });
+
+const initialState = [
+  ...AppInitData.workspaces,
+];
+
+function workspaces(state = initialState, action) {
+  switch (action.type) {
+
+    case ADD_WORKSPACE:
+      return [
+        ...state,
+        { ...action.workspace },
+      ];
+
+    case CHANGE_WORKSPACE_FAV:
+      return state.map(workspace =>
+        workspace.ws_id === action.ws_id ?
+          { ...workspace, is_fav: !workspace.is_fav } : workspace,
+      );
+
+    case DELETE_WORKSPACE:
+      return state.filter(workspace => workspace.ws_id !== action.ws_id);
+
+    default: return state;
+
+  }
+}
+
+export default workspaces;
