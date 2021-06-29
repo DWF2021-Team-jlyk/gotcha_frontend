8d2f2c882ccfb2e9dcd379de1921e00a7249d4fe
taeheon95 <dalo952003@gmail.com>
2021-06-29 09:59:34 +0900

    fix main error

--- src/pages/workspace/WorkList.js
+++ src/pages/workspace/WorkList.js
@@ -1,82 +1,80 @@
-import React, { useEffect, useState } from "react";
-import Button from "@material-ui/core/Button";
-import MyList from "../../components/WorkList/WorkListCardList";
+import React, { useEffect, useState } from 'react';
+import Button from '@material-ui/core/Button';
 import axios from 'axios';
-import WorkSpaceData from "../../DummyData/WorkSpaceData";
+import WorkSpaceData from '../../DummyData/WorkSpaceData';
 import { useDispatch } from 'react-redux';
 import { postList } from '../../modules/workspaceList';
+import WorkListCardList from './components/worklist/WorkListCardList';
 
 const listStyle = {
-    width: "100%",
-    height: "100%",
-    display: "flex",
-    overflowX: "auto",
-    margin: "10px",
-    whiteSpace: "nowrap"
+  width: '100%',
+  height: '100%',
+  display: 'flex',
+  overflowX: 'auto',
+  margin: '10px',
+  whiteSpace: 'nowrap',
 };
 
 const WorkList = () => {
-    const [lists, setLists] = useState([
+  const [lists, setLists] = useState([]);
 
-    ]);
-    
-    const url = '/workspace2'
-    const datas = {
-        "ws_id": 1,
-        "list_id": 1,
-        "token": sessionStorage.getItem('authorization'),
-    };
-    const options = {
-        method: 'POST',
-        headers: { 'content-type': 'application/json' },
-        data: JSON.stringify(datas),
-        url,
-    };
+  const url = '/workspace2';
+  const datas = {
+    'ws_id': 1,
+    'list_id': 1,
+    'token': sessionStorage.getItem('authorization'),
+  };
+  const options = {
+    method: 'POST',
+    headers: { 'content-type': 'application/json' },
+    data: JSON.stringify(datas),
+    url,
+  };
 
-    axios(options)
-        .then((res) => {
-            console.log(res)
-        }, [])
-        .catch(error => {
-            console.log(error)
-        });
+  axios(options)
+    .then((res) => {
+      console.log(res);
+    }, [])
+    .catch(error => {
+      console.log(error);
+    });
 
 
+  return (
+    <>
+      {/*<Button*/}
+      {/*  variant='contained'*/}
+      {/*  color='primary'*/}
+      {/*  onClick={(e) => {*/}
+      {/*    setLists([*/}
+      {/*      ...lists,*/}
+      {/*      { title: 'listTest', cards: ['test1', 'test2', 'test3'] },*/}
+      {/*    ]);*/}
+      {/*  }}*/}
+      {/*>*/}
+      {/*  + Add Another List*/}
+      {/*</Button>*/}
 
-    return (
-        <>
-            {/* <Button
-                variant="contained"
-                color="primary"
-                onClick={(e) => {
-                    setLists([
-                        ...lists,
-                        {title: "listTest", cards: ["test1", "test2", "test3"]},
-                    ]);
-                }}
-            >
-                + Add Another List
-            </Button>
+      {/*<div style={listStyle}>*/}
+      {/*  {lists.map((list, index) => {*/}
+      {/*    return <div key={index}>*/}
+      {/*      <WorkListCardList*/}
+      {/*        lists={lists}*/}
+      {/*        list={list}*/}
+      {/*        listId={list.list_id}*/}
+      {/*        cards={cards}*/}
+      {/*        // setList={setLists}*/}
+      {/*      />*/}
+      {/*    </div>;*/}
+      {/*  })}*/}
+      {/*  <div style={{ margin: 10 }}>*/}
 
-            <div style={listStyle}>
-               {lists.map((list, index) => {
-                   return <div key={index}>
-                       <WorkListCardList
-                           lists={lists}
-                           list={list}
-                           listId={list.list_id}
-                           cards={cards}
-                           // setList={setLists}
-                       />
-                   </div>
-               })}
-               <div style={{margin: 10}}>
+      {/*  </div>*/}
+      {/*</div>*/}
 
-               </div>
-            </div>
-            
-        </>
-    );
-        };
+    </>
+  );
+};
+;
 
 export default WorkList;
