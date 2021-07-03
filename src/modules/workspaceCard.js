765c40b75eed923bd34f5ebbc891ebbc7c915e0f
taeheon95 <dalo952003@gmail.com>
2021-07-03 18:11:31 +0900

    card reducer 함수 변화 redux-actions랑 immer를 사용하여 코드를 리팩토링함.

--- src/modules/workspaceCard.js
+++ src/modules/workspaceCard.js
@@ -1,18 +1,28 @@
 import * as api from '../lib/workListAPI';
 import createRequest from '../lib/createRequest';
+import { handleActions } from 'redux-actions';
+import produce from 'immer';
 
 const POST_CARD = 'workspace/POST_CARD';
 const POST_CARD_SUCCESS = 'workspace/POST_CARD_SUCCESS';
 const POST_CARD_FAILURE = 'workspace/POST_CARD_FAILURE';
 
 const CARD_ADD = 'workspace/CARD_ADD';
-const CARD_UPDATE = 'workspace/CARD_UPDATE';
+const CARD_ADD_SUCCESS = 'workspace/CARD_ADD_SUCCESS';
+const CARD_ADD_FAILURE = 'workspace/CARD_ADD_FAILURE';
+
+const CARD_UPDATE = 'workspace/CARD_UPDATE'
+const CARD_UPDATE_SUCCESS = 'workspace/CARD_UPDATE_SUCCESS';
+const CARD_UPDATE_FAILURE = 'workspace/CARD_UPDATE_FAILURE';
+
 const CARD_DELETE = 'workspace/CARD_DELETE';
+const CARD_DELETE_SUCCESS = 'workspace/CARD_DELETE_SUCCESS';
+const CARD_DELETE_FAILURE = 'workspace/CARD_DELETE_FAILURE';
 
 export const postCard = createRequest(POST_CARD, api.postCard);
-export const cardAdd = card =>({type:CARD_ADD, card});
-export const cardUpdate = card =>({type:CARD_UPDATE, card});
-export const cardDelete = card_id =>({type:CARD_DELETE, card_id});
+export const cardAdd = createRequest(CARD_ADD, api.addCard);
+export const cardUpdate = createRequest(CARD_UPDATE, api.updateCard);
+export const cardDelete = createRequest(CARD_DELETE, api.deleteCard);
 
 const initialState = {
   loading: {
@@ -21,59 +31,65 @@ const initialState = {
   cards: [],
 };
 
-function workspaceCard(state = initialState, action) {
-    switch (action.type) {
-      case POST_CARD:
-        return {
-          ...state,
-          loading: {
-            ...state.loading,
-            POST_CARD: true,
-          },
-        };
-      case POST_CARD_SUCCESS:
-        return {
-          ...state,
-          loading: {
-            ...state.loading,
-            POST_CARD: false,
-          },
-          cards: action.payload,
-        };
-      case POST_CARD_FAILURE:
-        return {
-          ...state,
-          loading: {
-            ...state.loading,
-            POST_CARD: false,
-          },
-        };
+const workspaceCard = handleActions(
+  {
+    [POST_CARD]: (state, action) =>
+      produce(state, draft => {
+        draft.loading.POST_CARD = true;
+      }),
+    [POST_CARD_SUCCESS]: (state, action) =>
+      produce(state, draft => {
+        draft.loading.POST_CARD = false;
+        draft.cards = action.payload;
+      }),
+    [POST_CARD_FAILURE]: (state, action) =>
+      produce(state, draft => {
+        draft.loading.POST_CARD = false;
+      }),//이까진 성공
+    [CARD_ADD]: (state, action) =>
+      produce(state, draft => {
+        draft.loading.POST_CARD = true;
+      }),
+    [CARD_ADD_SUCCESS]: (state, action) =>
+      produce(state, draft => {
+        draft.loading.POST_CARD = false;
+        draft.cards.push(action.payload);
+      }),
+    [CARD_ADD_FAILURE]: (state, action) =>
+      produce(state, draft => {
+        draft.loading.POST_CARD = false;
+      }),
+    [CARD_UPDATE]: (state, action) =>
+      produce(state, draft => {
+        draft.loading.POST_CARD = true;
+      }),
+    [CARD_UPDATE_SUCCESS]: (state, action) =>
+      produce(state, draft => {
+        draft.loading.POST_CARD = false;
+        draft.cards.find(card=> card.card_id === action.payload.card_id);
+      }),
+    [CARD_UPDATE_FAILURE]: (state, action) =>
+      produce(state, draft => {
+        draft.loading.POST_CARD = false;
+      }),
+    [CARD_DELETE]: (state, action) =>
+      produce(state, draft => {
+        draft.loading.POST_CARD = true;
+      }),
+    [CARD_DELETE_SUCCESS]: (state, action) =>
+      produce(state, draft => {
+        draft.loading.POST_CARD = false;
+        const index = draft.cards
+          .findIndex(card=>card.card_id === action.payload.card_id);
+        draft.cards.splice(index,1);
+      }),
+    [CARD_DELETE_FAILURE]: (state, action) =>
+      produce(state, draft => {
+        draft.loading.POST_CARD = false;
+      }),
+  },
+  initialState,
+);
 
-      case CARD_ADD:
-        return{
-          ...state,
-          cards: state.cards.concat(action.card)
-        };
-      
-        case CARD_UPDATE:
-          return{
-            ...state,
-            cards: state.cards.map(
-             card => card.card_id === action.card.card_id?
-             {...action.card} : card 
-            )
-          };
-
-        case CARD_DELETE:
-          return{
-            ...state,
-            cards: state.cards.filter(
-              card => card.card_id !== action.card_id 
-            )
-          };
-        default:
-          return state;
-    };
-  };
   
   export default workspaceCard;
